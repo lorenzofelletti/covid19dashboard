2830061bda6133df84cbf864f284b5bbf8f9bf11
lorenzofelletti <lorenzo97.felletti@gmail.com>
2020-10-31 13:43:02 +0100

    icon changes

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -10,7 +10,15 @@ function NavigationBar(props) {
 
   return (
     <Navbar id="navigation-bar" collapseOnSelect sticky="top" expand="lg" bg="light" variant="light">
-      <Navbar.Brand href={`${props.basename}`}>Covid19Dashboard</Navbar.Brand>
+      <Navbar.Brand href={`${props.basename}`}>
+        <img
+          src={`${props.basename}/logo.gif`}
+          width="120"
+          height="40"
+          className="d-inline-block align-top"
+          alt="covid19dashboard"
+        />
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
