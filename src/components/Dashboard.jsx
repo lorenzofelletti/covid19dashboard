c45e91c39acb35d8c5dece628b52bc277aa455c8
lorenzofelletti <lorenzo97.felletti@gmail.com>
2020-11-06 22:38:55 +0100

    added style for dark theme

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,17 +3,68 @@ import { Tabs, Tab } from 'react-bootstrap';
 import './Dashboard.css';
 import Historical from './Historical';
 import Daily from './Daily';
+import { darkTheme } from './Themes'
 
 function Dashboard(props) {
+
+  let tabsClassName = 'mt-3 ';
+  let tabClassName = '';
+  if (props.theme === 'dark') {
+    tabsClassName += 'dark-tabs';
+    tabClassName += 'dark-tab ';
+  }
   return (
     <>
-    <Tabs defaultActiveKey="cumulative" id="dash-tabs"  className='mt-3'>
-      <Tab eventKey="cumulative" title="Cumulative">
-        <Historical theme={props.theme}/>
-      </Tab>
-      <Tab eventKey="daily" title="Daily">
-        <Daily theme={props.theme}/>
-      </Tab>
+      <>
+        <style type="text/css">
+          {`
+          .dark-tabs {
+            background-color: ${darkTheme.backround};
+            color: ${darkTheme.text};
+            border-radius: .25rem;
+            border-color: gray;
+          }
+
+          .dark-tab {
+            border-radius: .25rem;
+          }
+          .dark-tab, .nav-link.active {
+            color: ${darkTheme.text};
+            
+          }
+
+          a {
+            color: ${darkTheme.text}
+          }
+          a:hover {
+            color: ${darkTheme.text}
+          }
+
+          .nav-tabs .nav-item.show .nav-link, .nav-tabs .nav-link.active {
+            border-radius: 0.25rem;
+          }
+          `}
+        </style>
+      </>
+      <Tabs
+        defaultActiveKey="cumulative"
+        id="dash-tabs"
+        className={tabsClassName}
+      >
+        <Tab
+          eventKey="cumulative"
+          title="Cumulative"
+          className={tabClassName}
+        >
+          <Historical theme={props.theme} />
+        </Tab>
+        <Tab
+          eventKey="daily"
+          title="Daily"
+          className={tabClassName}
+        >
+          <Daily theme={props.theme} />
+        </Tab>
       </Tabs>
     </>
   );
