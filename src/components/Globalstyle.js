73000c9a9b11fabe11d9ff823200b2e7ff632946
lorenzofelletti <lorenzo97.felletti@gmail.com>
2021-02-21 18:55:32 +0100

    formatted with Prettier

--- src/components/Globalstyle.js
+++ src/components/Globalstyle.js
@@ -5,7 +5,8 @@ export const GlobalStyles = createGlobalStyle`
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
   }
-  `
+  `;
 
-  /*font-family: Tahoma, Helvetica, Arial, Roboto, sans-serif;
-  transition: all 0.50s linear;*/
+/* font-family: Tahoma, Helvetica, Arial, Roboto, sans-serif;
+ * transition: all 0.50s linear;
+ */
--- src/components/Themes.js
+++ src/components/Themes.js
@@ -1,13 +1,13 @@
 export const lightTheme = {
-  body: '#FFF',
-  text: '#101010',
-  toggleBorder: '#FFF',
-  backround: '#363537'
-}
+  body: "#FFF",
+  text: "#101010",
+  toggleBorder: "#FFF",
+  backround: "#363537",
+};
 
 export const darkTheme = {
-  body: '#121212', //'#363537',
-  text: '#FAFAFA',
-  toggleBorder: '#6B8096',
-  backround: '#363537'
-}
+  body: "#121212", //'#363537',
+  text: "#FAFAFA",
+  toggleBorder: "#6B8096",
+  backround: "#363537",
+};
