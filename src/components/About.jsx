053d76cf626fae65556686d84ec2f3ae4ddd8507
lorenzofelletti <lorenzo97.felletti@gmail.com>
2021-02-21 23:45:49 +0100

    updated About

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,12 +5,12 @@ export const About = () => (
   <>
     <h1>About</h1>
     <h2>
-      Covid 19 Dashboard
+      Covid-19 Dashboard
     </h2>
     <p>
-      See the current Covid 19 situation.
+      See the current Covid-19 situation.
     </p>
-    <h3>Whoami</h3>
+    <h3>whoami</h3>
     <p>My name is <a href="https://lorenzofelletti.github.io">Lorenzo Felletti</a> and I'm a computer engineering student ad the University of Bologna.</p>
     <p>Source code <a href="https://github.com/lorenzofelletti/covid19dashboard">here</a>.</p>
   </>
