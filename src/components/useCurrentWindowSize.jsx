eae1d5b82569799398d2522a421b897e5443e50f
lorenzofelletti <lorenzo97.felletti@gmail.com>
2021-03-10 02:02:04 +0100

    hook to get current window size

---
+++ src/components/useCurrentWindowSize.jsx
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
+}
+
+export default function useCurrentWindowSize() {
+  // save current window width in the state object
+  const [size, setSize] = useState(getWindowDimensions());
+
+  // in this case useEffect will execute only once because
+  // it does not have any dependencies.
+  useEffect(() => {
+    // timeoutId for debounce mechanism
+    let timeoutId = null;
+    const resizeListener = () => {
+      // prevent execution of previous setTimeout
+      clearTimeout(timeoutId);
+      // change width from the state object after 150 milliseconds
+      timeoutId = setTimeout(() => setSize(getWindowDimensions()), 150);
+    };
+    // set resize listener
+    window.addEventListener('resize', resizeListener);
+
+    // clean up function
+    return () => {
+      // remove resize listener
+      window.removeEventListener('resize', resizeListener);
+    };
+  }, []);
+
+  return size;
+}
