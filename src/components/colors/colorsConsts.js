4c342cf2fe11b7a34e9f42c9e13b6727ed53e7e7
lorenzofelletti <lorenzo97.felletti@gmail.com>
2021-03-10 01:56:41 +0100

    separated color constants

--- src/components/colors/colorsConsts.js
+++ src/components/colors/colorsConsts.js
@@ -1,7 +1,7 @@
 const categoryColor = {
-  cases: "#FF0000",
-  deaths: "#FFAD05", //"#252525",
-  recovered: "#62FF00"
-}
+  cases: '#FF0000',
+  deaths: '#FFAD05', // "#252525",
+  recovered: '#62FF00',
+};
 
 export default categoryColor;
